0971f701a925cdb1d807692e68b030a04eff38e8
Rebeca Calazans <rebecac.brito@gmail.com>
2018-03-07 10:19:45 -0300

    alterado problema b da lista 03, imepp 2018

--- imepp/2018/lista03/b.cpp
+++ imepp/2018/lista03/b.cpp
@@ -1,32 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define st first
-#define nd second
-#define mp make_pair
-#define pb push_back
-#define cl(x, v) memset((x), (v), sizeof(x))
-
-#define db(x) cerr << #x << " == " << x << endl
-#define dbs(x) cerr << x << endl
-#define _ << ", " <<
-
-typedef long long ll;
-typedef long double ld;
-
-typedef pair<int, int> pii;
-typedef pair<int, pii> piii;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
-
-const ld EPS = 1e-9, PI = acos(-1.);
-const int INF = 0x3f3f3f3f, MOD = 1e9+7;
-const int N = 1e5+5;
-
 int coord[3][2];
 
 bool is_right_angled() {
-  vi squared_sides;
+  vector<int> squared_sides;
+  int sum = 0, ma = 0;
 
   for(int i=0; i<3; i++) {
     for(int j=i+1; j<3; j++) {
@@ -34,8 +13,6 @@ bool is_right_angled() {
       squared_sides.push_back(dx*dx + dy*dy);
     }
   }
-
-  int sum = 0, ma = 0;
 
   for(int s: squared_sides) {
     sum += s;
@@ -57,21 +34,16 @@ int main() {
   }
 
   for(int i=0; i<3; i++) for(int j=0; j<2; j++) {
-
     coord[i][j] += 1;
     if(is_right_angled()) {
-      db(i _ j _ 1);
       printf("ALMOST\n");
       return 0;
     }
-
     coord[i][j] -= 2;
     if(is_right_angled()) {
-      db(i _ j _ -1);
       printf("ALMOST\n");
       return 0;
     }
-
     coord[i][j] += 1;
   }
 
